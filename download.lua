1ddf2dca2d2127c298545b6063285ea49d4be487
Tumkov <107698704+Tumkov@users.noreply.github.com>
2022-11-02 23:47:12 +0200

    Update download.lua

--- download.lua
+++ download.lua
@@ -1,6 +1,6 @@
 local REPOSITOTY = "https://raw.githubusercontent.com/Tumkov/bleb/main"
 
 local shell = require("shell")
-shell.execute("wget -f " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
+shell.execute("wget -f " .. REPOSITOTY .. "/launcher.lua /home/start.lua")
 shell.execute("wget -f " .. REPOSITOTY .. "/libs/casino.lua /lib/casino.lua")
 shell.execute("wget -f " .. REPOSITOTY .. "/config/settings.lua /lib/settings.lua")
