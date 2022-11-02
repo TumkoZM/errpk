3c5fb77190bc7110b5af2518b4db2d5ef5e8f0ab
Tumkov <107698704+Tumkov@users.noreply.github.com>
2022-11-03 00:39:30 +0200

    Update games.lua

--- config/games.lua
+++ config/games.lua
@@ -1,7 +1,14 @@
 local games = {}
 
 
-
+table.insert(games, {
+    title = "OpenChest",
+    file = "game_OpenChest.lua",
+    available = false,
+    image = "game_Chests.pic",
+    author = "Durex77",
+    description = ""
+})
 
 
 
