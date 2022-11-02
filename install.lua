75e7ed2a52c4233cc2398b6a3334c27a962bd511
Tumkov <107698704+Tumkov@users.noreply.github.com>
2022-11-02 23:36:13 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
-local REPOSITOTY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
+local REPOSITOTY = "https://raw.githubusercontent.com/Tumkov/bleb/main"
 
 local shell = require("shell")
-shell.execute("wget -fq " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/libs/casino.lua /lib/casino.lua")
-shell.execute("wget -fq " .. REPOSITOTY .. "/config/settings.lua /lib/settings.lua")
+shell.execute("wget -f " .. REPOSITOTY .. "/launcher.lua /home/1.lua")
+shell.execute("wget -f " .. REPOSITOTY .. "/libs/casino.lua /lib/casino.lua")
+shell.execute("wget -f " .. REPOSITOTY .. "/config/settings.lua /lib/settings.lua")
 shell.execute("edit /lib/settings.lua")
