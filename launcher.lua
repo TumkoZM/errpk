318c99a8048a496c4ca48e6f7a6aa60c27054ce0
Tumkov <107698704+Tumkov@users.noreply.github.com>
2022-11-04 14:57:33 +0200

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -9,7 +9,6 @@ local currencies
 local image
 local buffer
 local colorlib
-local pim=require("component").pim
  
 REPOSITORY = settings.REPOSITORY
 
@@ -143,7 +142,7 @@ function getJBQty()
     local inventorySize = 36
     local slot = {}
     for i = 1, inventorySize do
-        slot[i] = pim.getStackInSlot(i)
+        slot[i] = casino.container.getStackInSlot (i)
         if slot[i] ~= nil then
             if slot[i].display_name == "Железный блок" or slot[i].display_name == "Block of Iron" then
                 JBqty = JBqty + slot[i].qty
