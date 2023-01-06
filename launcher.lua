0d48a53583eebeefff395544df9db34930e5bf01
Tumkov <107698704+Tumkov@users.noreply.github.com>
2023-01-06 02:13:36 +0200

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -1,4 +1,4 @@
-local casino = require("pril")--1:26did naxyi
+local casino = require("pril")--2:13did naxyi
 local event = require("event")
 local shell = require("shell")
 local unicode = require("unicode")
