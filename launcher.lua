2513f1bd78f403cdeae126766ca946844abf855b
Tumkov <107698704+Tumkov@users.noreply.github.com>
2023-01-07 01:52:47 +0200

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -667,9 +667,6 @@ while true do
   elseif e[1] == "scroll" then
     scroll(e[5])
   elseif e[1] == "touch" then
-    gpu.setBackground(0x000000)
-    gpu.setForeground(0xFFFFFF)
-    gpu.set(1,1,e[3].."  "..e[4].." ")
     choice = false
     for i = 1,#pos_str do
       if e[3] <= 77 and e[4] == pos_str[i][1] then
