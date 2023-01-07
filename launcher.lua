21de63d5eca09720e73552726f306ba714efd634
Tumkov <107698704+Tumkov@users.noreply.github.com>
2023-01-07 02:19:49 +0200

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -277,8 +277,27 @@ end
   end
   drawlist()
 end
-
-
+while run do
+  local e = {event.pull(1)}
+  if e[1] == "key_down" then
+    if e[4] == 29 then
+      run = false
+    elseif e[4] == 200 then
+      scroll("+")
+    elseif e[4] == 208 then
+      scroll("-")
+    end
+  elseif e[1] == "scroll" then
+    scroll(e[5])
+elseif e[1] == "touch" then
+    choice = false
+    for i = 1,#pos_str do
+      if e[3] <= 77 and e[4] == pos_str[i][1] then
+        choice = pos_str[i][2]
+        break
+      end
+    end
+end
 function getJBQty()
     local JBqty = 0
     local inventorySize = 36
