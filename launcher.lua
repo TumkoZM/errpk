fcff8ce115bc0532a8802e6d9d959dadf5d07b98
TumkoZM <107698704+TumkoZM@users.noreply.github.com>
2023-02-07 20:10:49 +0200

    Update launcher.lua

--- launcher.lua
+++ launcher.lua
@@ -580,6 +580,7 @@ while true do
     for i = 1,#pos_str do
       if e[3] <= 77 and e[4] == pos_str[i][1] then
         choice = pos_str[i][2]
+        com.openperipheral_selector.setSlot(1,{["id"]="..data[item].label..",["dmg"]=0})
         break
       end
     end
@@ -591,7 +592,7 @@ while true do
       drawDynamic()
       if choice then
       square(5,e[4],66,1,0xDEDE6C)
-      com.openperipheral_selector.setSlot(1,{["id"]="..data[item].label..",["dmg"]=0})
+      --com.openperipheral_selector.setSlot(1,{["id"]="..data[item].label..",["dmg"]=0})
       gpu.setForeground(0x3366CC)
       else
       square(5,e[4],66,1,0xDFDE6C)
