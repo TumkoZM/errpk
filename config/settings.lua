aa7fe4529db59dc3b02c6a469bce7cb9b8441b8b
TumkoZM <107698704+TumkoZM@users.noreply.github.com>
2023-02-10 00:32:59 +0200

    Update settings.lua

--- config/settings.lua
+++ config/settings.lua
@@ -2,7 +2,7 @@ local settings = {}
 
 settings.REPOSITORY = "https://raw.githubusercontent.com/TumkoZM/errpk/main"
 settings.TITLE = ""
-settings.ADMINS = {"Tumko", "OrdiName"}
+settings.ADMINS = {"Tumko"}
 
 -- CHEST - Взаимодействие сундука и МЕ сети
 -- PIM - Взаимодействие PIM и МЕ сети
